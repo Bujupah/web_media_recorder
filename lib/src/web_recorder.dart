6e3d37c27235c869330e51eeba3ceff830939b59
Khalil Mejdi <khalil.mejdi97@gmail.com>
2020-01-28 12:23:26 +0100

    updates

--- lib/src/web_recorder.dart
+++ lib/src/web_recorder.dart
@@ -5,19 +5,53 @@ import 'dart:js' as js;
 
 import 'package:flutter/material.dart';
 
+
+/// This class is using [html.MediaRecorder] natively and parse its data to the flutter
+/// ```
+/// class WebRecorder {
+///   static bool isNotRecording = true;
+/// 
+///   final Function whenRecorderStart; 
+///   final Function whenRecorderStop;
+///   final Function whenReceiveData;
+///   
+///   openRecorder();
+/// }
+/// ```
 class WebRecorder {
   static bool isNotRecording = true;
-  static html.AudioElement audioElement;
   static html.MediaRecorder recorder;
 
-  final Function whenRecorderStart; // Function to call when recording starts
-  final Function whenRecorderStop; // Function to call when recording finishs
+  final Function whenRecorderStart; 
+  final Function whenRecorderStop;
   final Function whenReceiveData;
 
-  WebRecorder({@required this.whenRecorderStart, @required this.whenRecorderStop, @required this.whenReceiveData}){
-    WebRecorder.audioElement = html.AudioElement();
-  }
 
+  /// @param whenRecorderStart is the function to use when recording starts.
+  /// @param whenRecorderStop is the function to use when recording finishs.
+  /// @param whenReceiveData is the function to use to return data from.
+  /// 
+  WebRecorder({@required this.whenRecorderStart, @required this.whenRecorderStop, @required this.whenReceiveData});
+
+  /// A switcher function to start/stop audio recording.
+  /// ```
+  /// openRecorder(){
+  ///   WebRecorder.isNotRecording = !WebRecorder.isNotRecording;
+  ///   if(WebRecorder.isNotRecording)
+  ///     stopRecoring().whenComplete(whenRecorderStop);
+  ///   else
+  ///     html.window.navigator
+  ///     .getUserMedia(audio: true)
+  ///     .then((stream) {
+  ///         recorder = html.MediaRecorder(stream);
+  ///         recorder.addEventListener('dataavailable', hundlerFunctionStream);
+  ///     })
+  ///     .whenComplete((){
+  ///       startRecording().whenComplete(whenRecorderStart);
+  ///     })
+  ///     .catchError((e)=> print);
+  /// }
+  /// ```
   openRecorder(){
     WebRecorder.isNotRecording = !WebRecorder.isNotRecording;
     if(WebRecorder.isNotRecording)
@@ -45,6 +79,20 @@ class WebRecorder {
     return Future.value(true);
   }
 
+  /// A function hundler that waits for [html.MediaStream] data
+  /// and then transform it to a [html.Blob] with [js.JsObject]
+  /// subsequently read it with [html.FileReader] to transform
+  /// into [Uint8List] data
+  /// ```  
+  /// hundlerFunctionStream(event) async{
+  ///   html.FileReader reader = html.FileReader();
+  ///   html.Blob blob = js.JsObject.fromBrowserObject(event)['data'];
+  ///   reader.readAsArrayBuffer(blob);
+  ///   reader.onLoadEnd.listen((e) async {
+  ///     setData(reader.result);
+  ///   });
+  /// }
+  /// ```
   hundlerFunctionStream(event) async{
     html.FileReader reader = html.FileReader();
     html.Blob blob = js.JsObject.fromBrowserObject(event)['data'];
@@ -54,10 +102,11 @@ class WebRecorder {
     });
   }
 
+  /// Send [html.AudioRecorder] data to the created function [whenReceiveData].
   setData(data) => whenReceiveData(data);
 
+  /// Dispose this [html.MediaRecorder]
   dispose(){
-    WebRecorder.audioElement = null;
     WebRecorder.recorder.removeEventListener('dataavailable', hundlerFunctionStream);
     WebRecorder.recorder = null;
   }
